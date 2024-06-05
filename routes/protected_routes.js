19afe6587af6182eee84bb8f5879a2b486e2cf93
elyaraujo <christie.araujo@gmail.com>
2024-06-04 22:16:12 -0300

    protectRoutes

--- routes/protected_routes.js
+++ routes/protected_routes.js
@@ -1,6 +1,15 @@
 import express from "express";
 const protectedRouter = express.Router();
 
+const validateToken = (request, response, next) => {
+//pegar token dos headers da request
+//validar o token
+//buscar o usuario no banco de dados com base no id que está no token
+//qualquer erro retornar 400
+
+  next();
+}
+
 // [GET] - http://localhost:3000/dashboard
 protectedRouter.get("/", (request, response) => {
   response.send("Listagem dos dados protegidos");
